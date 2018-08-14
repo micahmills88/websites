1756f0ca6304d78c29399bf8183dd0863b396222
micahmills88 <mdmills88@gmail.com>
2018-08-14 17:03:14 -0400

    added passport activedirectory stub code

--- automation-builder/backend/passport/localstrategy.js
+++ automation-builder/backend/passport/localstrategy.js
@@ -1,6 +1,14 @@
 import User from '../models/user';
 const LocalStrategy = require('passport-local').Strategy;
 
+//declare activedirectory stuff right here
+// var ActiveDirectory = require('activedirectory');
+// var ad = new ActiveDirectory({ url: 'ldap://dc.domain.com',
+//     baseDN: 'dc=domain,dc=com',
+//     username: 'username@domain.com',
+//     password: 'password'
+// });
+
 const strategy = new LocalStrategy(
 	{
 		usernameField: 'username' // not necessary, DEFAULT
@@ -9,8 +17,20 @@ const strategy = new LocalStrategy(
         console.log(username);
 		User.findOne({ user_name: username}, (err, res) => {
 			if(err) return done(err);
-			
-			//verify user via activedirectory
+			//verify user via activedirectory above
+			// ad.authenticate(username, password, function(err, isAuthenticated) {
+			// 	if(err) return done(err, null);
+			// 	if(isAuthenticated) {
+			// 		return done(null, {
+			// 			username: username
+			// 		});
+			// 	}
+			// 	else {
+			// 		return done(null, false);
+			// 	}
+			// });
+
+
 			return done(null, res);
 		});
 	}
